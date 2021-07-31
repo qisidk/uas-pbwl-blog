d678304617d4852dda5ca7747271f8a43546d74c
qisidk <86147149+qisidk@users.noreply.github.com>
2021-07-31 22:13:18 +0700

    Add files via upload

---
+++ database/migrations/2021_07_27_045944_create_photo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo', function (Blueprint $table) {
+            $table->increments('id_photo');
+            $table->string('judul_photo');
+            $table->string('text_photo');
+            $table->date('tanggal_photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo');
+    }
+}
